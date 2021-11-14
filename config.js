99f3cddb2a5fb65a90edfca9d1009d7d95a3386c
Hoang An Unicode <hoanganit19@gmail.com>
2021-11-14 10:50:57 +0700

    Update config

--- config.js
+++ config.js
@@ -7,12 +7,4 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
-    config.filebrowserBrowseUrl = '/phpk5/ckfinder/ckfinder.html';
-
-    config.filebrowserImageBrowseUrl = '/phpk5/ckfinder/ckfinder.html?type=Images';
-
-    config.filebrowserUploadUrl = '/phpk5/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-
-    config.filebrowserImageUploadUrl = '/phpk5/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
 };
